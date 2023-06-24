9c3486f532aac5c122fa47081e0d8eec12244619
Deepjyoti23 <deepjyoti0401@gmail.com>
2023-06-25 01:01:57 +0530

    innerHTML

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-let myLeads = ['hh'];
+let myLeads = ["www.awesome.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el")
-const inputBtn =document.getElementById("input-btn")
-const ulEl=document.getElementById("ul-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click",function(){
+inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
     console.log(myLeads);
 })
 
-for(let i=0; i<myLeads.length; i++){
-    console.log(myLeads[i]);
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    // console.log(myLeads[i]);
 }
