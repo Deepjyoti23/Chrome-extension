3cd568ce4ed95a6e06703711d86ec23dd7c1283c
Deepjyoti23 <deepjyoti0401@gmail.com>
2023-06-24 21:43:11 +0530

    init

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function saveLead(){
-    console.log("Button Clicked");
+let myLeads = ['hh'];
+const inputEl = document.getElementById("input-el")
+const inputBtn =document.getElementById("input-btn")
+const ulEl=document.getElementById("ul-el")
+
+inputBtn.addEventListener("click",function(){
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
+})
+
+for(let i=0; i<myLeads.length; i++){
+    console.log(myLeads[i]);
 }
