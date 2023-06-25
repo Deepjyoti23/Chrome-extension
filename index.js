caf9bb644babe5226d4201ec8b7799bb5f6fe105
Deepjyoti23 <deepjyoti0401@gmail.com>
2023-06-25 16:45:19 +0530

    add anchor tag

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-let myLeads = ["www.awesome.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    inputEl.value = "";
+    renderLeads();
 })
+function renderLeads() {
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    // console.log(myLeads[i]);
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li><a href='" + myLeads[i] + "' target = '_blank'>" + myLeads[i] + "</a></li>";
+        //first way of doing this
+        //  ulEl.innerHTML += "<li><a href='#'>" + myLeads[i] + "</a></li>"
+        // console.log(myLeads[i]);
+        //second way
+        //create element
+        //set text content
+        //append to ul
+        // const li =document.createElement("li")
+        // li.textContent = myLeads[i];
+        // ulEl.append(li)
+    }
+    ulEl.innerHTML = listItems;
 }
